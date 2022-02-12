8a257192d62e7764ea6b2258d66bc19dc24825fe
Bryonpectol <bryon.pectol@gmail.com>
2022-02-11 18:11:00 -0700

    added array for items

---
+++ js/store.js
@@ -0,0 +1,12 @@
+const storeItems = [
+    {
+        "name": "plastic Dress",
+        "img": "",
+        "storelink": "https://square.link/u/v1j0yV00",
+    },
+    {
+        "name": "plastic Dress",
+        "img": "",
+        "storelink": "https://square.link/u/v1j0yV00",
+    },
+]
