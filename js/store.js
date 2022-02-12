8e3e0a96b77a8cb63b99cfea1a2b2b696b32abbc
Bryonpectol <bryon.pectol@gmail.com>
2022-02-12 06:30:39 -0700

    test login

--- js/store.js
+++ js/store.js
@@ -1,12 +1,56 @@
-const storeItems = [
-    {
-        "name": "plastic Dress",
-        "img": "",
-        "storelink": "https://square.link/u/v1j0yV00",
-    },
-    {
-        "name": "plastic Dress",
-        "img": "",
-        "storelink": "https://square.link/u/v1j0yV00",
-    },
-]
+import { storeData } from "../js/storedata.js";
+
+const mainContainer = document.createElement("div");
+mainContainer.className = "container";
+
+const allCars = storeData.map(myCar => {
+  let carsYeah = storeData.find(CAR => {
+    return CAR.name === myCar.name;
+  });
+
+  return {
+    // this is how the script finds the item in the array.
+    //   nAME: myCar.name,
+    mAKE: carsYeah.name,
+    Link: carsYeah.storelink,
+    image: carsYeah.img
+  };
+});
+
+allCars.forEach(myCar => {
+  
+  
+    // this section is to create the html elements.
+    let makeElement = document.createElement("div");
+    
+    let imageElement = document.createElement("img");
+    
+    let linkElement = document.createElement("a");
+
+    makeElement.className = "make";
+    
+    imageElement.className = "image";
+
+    linkElement.className = "link";
+  
+  
+    makeElement.textContent = myCar.mAKE;
+
+    linkElement.textContent = "Buy Now";
+   
+    imageElement.src = myCar.image;
+    linkElement.href = myCar.Link;
+    
+      
+    mainContainer.appendChild(makeElement);
+    
+    makeElement.appendChild(imageElement);
+    makeElement.appendChild(linkElement);
+  
+    
+    
+  });
+  
+  document.body.appendChild(mainContainer);
+  
+  console.log(storeData);
---
+++ js/storedata.js
@@ -0,0 +1,12 @@
+export const storeData = [
+    {
+        "name":"Plastic Dress",
+        "img":"/images/plasticdress1.png",
+        "storelink":"https://square.link/u/JiwY4qsE"
+    },
+    {
+        "name":"Plastic Dress1",
+        "img":"",
+        "storelink":"no"
+    },
+];
